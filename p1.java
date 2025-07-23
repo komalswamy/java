10c9c445ef6c643f31a8cf1da3ac526df29e167e
komalswamy <2420080053@klh.edu.in>
2025-07-23 11:12:08 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class p1 {
+	public static void main(String args[])
+    {
+        int x = 2;
+        while (x <= 5)
+        {
+            System.out.println("hello");
+            x++;
+        }
+    }
+}
---
+++ p2.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class p2 {
+	class For
+	{
+	    public static void main(String args[])
+	    {
+	        int x = 2, i;
+	        for(i = 0; i < 5; i++)
+	        {
+	            System.out.println("hello");
+	        }
+	    }
+	}
+
+}
