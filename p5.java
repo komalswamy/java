fedb994e3402e4b0865e18277f93668311f160dd
komalswamy <2420080053@klh.edu.in>
2025-07-23 11:36:32 +0530

    Add files via upload

---
+++ p5.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class p5 {
+	public class TypeConversion {
+	    public static void main(String[] args) {
+	        int n = 100;
+	        double d = n; // int to double (implicit conversion)
+
+	        System.out.println("Integer value: " + n);
+	        System.out.println("Converted to double: " + d);
+	    }
+	}
+
+}
